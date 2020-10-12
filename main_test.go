5f2ebf4fdb4696e4ff2556de09abe3f8025d1dc5
Angelo Brouns <abrouns@garagesoftware.nl>
2020-10-12 21:46:48 +0200

    remove unused test

--- main_test.go
+++ main_test.go
@@ -21,21 +21,4 @@ func Test_FindProcValue(t *testing.T) {
 	}
 }
 
-func Test_getProcessInformation(t *testing.T) {
-	testString := "13936 (bash) S 1 13936 13936 196608 -1 0 3436 3436 0 0 78 359 78 359 8 0 0 0 854085461 7196672 2628 345"
-	resultProc := getProcessInformation()
-	result := findProcValue(testString, 0)
-	if result != "1" {
-		t.Fail()
-	}
 
-	result = findProcValue(testString, 4)
-	if result != "5" {
-		t.Fail()
-	}
-
-	result = findProcValue(testString, 100)
-	if result != "" {
-		t.Fail()
-	}
-}
